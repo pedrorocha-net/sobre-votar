066f4dd59aa15ec076e0d417167924ebef8a324f
“Brenno <dev.brennoeudes@gmail.com>
2024-03-17 12:02:39 -0300

    app usestate, handleInputChange, handleSubmit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import React, { useState } from "react";
+import query from "./api/query.js";
+
+
 function App() {
+  const [userInput, setUserInput] = useState(""); // Estado p/ armazenar o valor do input
+
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value); // Atualiza o estado com o valor inserido no input
+  };
+
+  const handleSubmit = async () => {
+    try {
+        // Envia REQ c/ os dados do input para a API e aguarda a RES
+        const response = await query({ prompt: userInput });
+        console.log(response); // Exibe a RES no console
+    } catch (error) {
+        console.error("Erro ao enviar requisição:", error);
+    }
+};
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello world!</p>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header"></header>
+
+        <div class="input-search">
+          <label for="user-question">Digite sua pergunta</label>
+          <input
+            id="user-question"
+            type="text"
+            placeholder="Digite sua pergunta"
+            value={userInput}
+            onChange={handleInputChange} // Add evento de escuta para capturar a mudança no input
+          />
+          <button onClick={handleSubmit}>Enviar</button>
+        </div>
+      </div>
+    </>
   );
 }
 
