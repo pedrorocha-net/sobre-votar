240636a549e45455d0b4ca0c3b7600df3c9b86c9
“Brenno <dev.brennoeudes@gmail.com>
2024-03-17 14:17:03 -0300

    html structured

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import query from "./api/query.js";
 
-
 function App() {
   const [userInput, setUserInput] = useState(""); // Estado p/ armazenar o valor do input
 
@@ -11,19 +10,23 @@ function App() {
 
   const handleSubmit = async () => {
     try {
-        // Envia REQ c/ os dados do input para a API e aguarda a RES
-        const response = await query({ prompt: userInput });
-        console.log(response); // Exibe a RES no console
+      // Envia REQ c/ os dados do input para a API e aguarda a RES
+      const response = await query({ prompt: userInput });
+      console.log(response); // Exibe a RES no console
     } catch (error) {
-        console.error("Erro ao enviar requisição:", error);
+      console.error("Erro ao enviar requisição:", error);
     }
-};
-
+  };
 
   return (
     <>
       <div className="App">
         <header className="App-header"></header>
+        <h1>Sobre Votar AI</h1>
+        <p>
+          Descubra todas as informações sobre a votação nas eleições de Portugal
+          em 2024, com a nossa Inteligência Artificial.
+        </p>
 
         <div class="input-search">
           <label for="user-question">Digite sua pergunta</label>
@@ -36,6 +39,19 @@ function App() {
           />
           <button onClick={handleSubmit}>Enviar</button>
         </div>
+
+        <div className="ai-logos">
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+
+        <footer>
+          <p>Sobre Votar © 2024. All Rights Reserved.</p>
+          <img src="" alt="" />
+        </footer>
       </div>
     </>
   );
